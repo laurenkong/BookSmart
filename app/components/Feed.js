04f8696a51225470ec5d3209f89c5b06a562a0bd
Kris Jeong <78010643+krisjeong@users.noreply.github.com>
2023-12-01 17:54:22 -0800

    fix Feed so it fetches data from BookInfo.js, not hard-coded

--- app/components/Feed.js
+++ app/components/Feed.js
@@ -5,155 +5,44 @@ import {
   StyleSheet,
   FlatList,
   TouchableOpacity,
+  ScrollView,
   Image,
   Pressable,
 } from "react-native";
-import Icon from "react-native-vector-icons/MaterialCommunityIcons";
+import { BookInfo } from "../../data/BookInfo";
 
 const ITEMS_PER_PAGE = 3; // Number of items to load per page
 
-const quotes = [
-  // Fetch from server later and randomize
-  {
-    id: 1,
-    text: '"It is a truth universally acknowledged, that a single man in possession of a good fortune, must be in want of a wife."',
-    bookData: {
-      volumeInfo: {
-        title: "Pride and Prejudice",
-        authors: ["Jane Austen"],
-        imageLinks: { thumbnail: require("../../assets/images/pandp.jpeg") },
-        description: "TODD: fill in",
-      },
-    },
-  },
-  {
-    id: 2,
-    text: '"It was the best of times, it was the worst of times, it was the age of wisdom, it was the age of foolishness, it was the epoch of belief, it was the epoch of incredulity, it was the season of light, it was the season of darkness, it was the spring of hope, it was the winter of despair."',
-    bookData: {
-      volumeInfo: {
-        title: "A Tale of Two Cities",
-        authors: ["Charles Dickens"],
-        imageLinks: {
-          thumbnail: require("../../assets/images/taleoftwocities.jpg"),
-        },
-        description: "TODD: fill in",
-      },
-    },
-  },
-  {
-    id: 3,
-    text: '"It was a bright cold day in April, and the clocks were striking thirteen."',
-    bookData: {
-      volumeInfo: {
-        title: "1984",
-        authors: ["George Orwell"],
-        imageLinks: { thumbnail: require("../../assets/images/1984.jpg") },
-        description: "TODD: fill in",
-      },
-    },
-  },
-  {
-    id: 4,
-    text: '"So we beat on, boats against the current, borne back ceaselessly into the past."',
-    bookData: {
-      volumeInfo: {
-        title: "The Great Gatsby",
-        authors: ["F. Scott Fitzgerald"],
-        imageLinks: { thumbnail: require("../../assets/images/gatsby.jpg") },
-        description: "TODD: fill in",
-      },
-    },
-  },
-  {
-    id: 5,
-    text: '"Her affection for him was now the breath and life of Tess’s being; it enveloped her as a photosphere, irradiated her into forgetfulness of her past sorrows, keeping back the gloomy spectres that would persist in their attempts to touch her — doubt, fear, moodiness, care, shame."',
-    bookData: {
-      volumeInfo: {
-        title: "Tess of the d'Urbervilles",
-        authors: ["Thomas Hardy"],
-        imageLinks: { thumbnail: require("../../assets/images/tess.jpg") },
-        description: "TODD: fill in",
-      },
-    },
-  },
-];
-
 const Feed = ({ navigation }) => {
-  const [data, setData] = useState([]);
-  const [page, setPage] = useState(1);
-  const [isEndReached, setIsEndReached] = useState(false);
-
-  useEffect(() => {
-    loadData();
-  }, [page]);
-
   const addItemToBookshelf = (bookData) => {
     // Placeholder function for adding a book to the bookshelf
     console.log("Added to bookshelf:", bookData.volumeInfo.title);
     // Implement the logic to add the book to the bookshelf here
   };
 
-  const loadData = () => {
-    // Simulate fetching data from server
-    const newData = quotes.slice(0, ITEMS_PER_PAGE * page);
-    if (newData.length < quotes.length) {
-      setData(newData);
-    } else {
-      setData(newData);
-      setIsEndReached(true); // No more data to load
-    }
-  };
-
-  const loadMoreData = () => {
-    if (!isEndReached) {
-      setPage(page + 1);
-    }
-  };
-
-  const renderItem = ({ item }) => (
-    <View style={styles.quoteCard}>
-      <Image
-        source={item.bookData.volumeInfo.imageLinks.thumbnail}
-        style={styles.thumbnail}
-      />
-      <View style={styles.textContainer}>
-        <Text style={styles.quote}>{item.text}</Text>
-        <View style={styles.buttonContainer}>
-          <Pressable
-            onPress={() =>
-              navigation.navigate("BookProfile", { bookData: item.bookData })
-            }
-          >
-            <Text style={styles.linkText}>Go to Book</Text>
-          </Pressable>
-          <Pressable onPress={() => navigation.navigate("Bookshelf")}>
-            <Text style={styles.linkText}>Add to Bookshelf</Text>
-          </Pressable>
+  const renderQuotes = (books) =>
+    books.map((book, index) => (
+      <View key={index} style={styles.quoteCard}>
+        <Image source={book.bookCover} style={styles.thumbnail} />
+        <View style={styles.textContainer}>
+          <Text style={styles.quoteContainer}>{book.content[0]}</Text>
+          <View style={styles.buttonContainer}>
+            <Pressable
+              onPress={() =>
+                navigation.navigate("BookProfile", { bookData: book })
+              }
+            >
+              <Text style={styles.linkText}>Go to Book</Text>
+            </Pressable>
+            <Pressable onPress={() => navigation.navigate("Bookshelf")}>
+              <Text style={styles.linkText}>Add to Bookshelf</Text>
+            </Pressable>
+          </View>
         </View>
       </View>
-    </View>
-  );
+    ));
 
-  const renderFooter = () => {
-    if (isEndReached) {
-      return (
-        <Text style={styles.endText}>You've reached the end of Discovery!</Text>
-      );
-    }
-    return null;
-  };
-
-  return (
-    <FlatList
-      data={data}
-      renderItem={renderItem}
-      keyExtractor={(item) => item.id.toString()}
-      onEndReached={loadMoreData}
-      onEndReachedThreshold={0.5} // Determines how far from the end to trigger the onEndReached callback
-      ListFooterComponent={renderFooter}
-      style={styles.container}
-    />
-  );
+  return <ScrollView>{renderQuotes(BookInfo)}</ScrollView>;
 };
 
 const styles = StyleSheet.create({
@@ -182,6 +71,11 @@ const styles = StyleSheet.create({
     flex: 1,
     alignItems: "flex-start",
     marginLeft: 5,
+    marginRight: 7,
+  },
+  quoteContainer: {
+    flex: 1,
+    alignItems: "flex-start",
   },
   linkText: {
     color: "#85C5A9",
--- data/BookInfo.js
+++ data/BookInfo.js
@@ -7,7 +7,8 @@ export const BookInfo = [
     title: "Pride and Prejudice",
     subtitle: "Chapter 1",
     content: [
-      "It is a truth universally acknowledged, that a single man in possession of a good fortune must be in want of a wife.\n\nHowever little known the feelings or views of such a man may be on his first entering a neighbourhood, this truth is so well fixed in the minds of the surrounding families, that he is considered as the rightful property of some one or other of their daughters.",
+      "It is a truth universally acknowledged, that a single man in possession of a good fortune must be in want of a wife.",
+      "However little known the feelings or views of such a man may be on his first entering a neighbourhood, this truth is so well fixed in the minds of the surrounding families, that he is considered as the rightful property of some one or other of their daughters.",
       '“My dear Mr. Bennet,” said his lady to him one day, “have you heard that Netherfield Park is let at last?"\nMr. Bennet replied that he had not.\n“But it is,” returned she; “for Mrs. Long has just been here, and she told me all about it."\nMr. Bennet made no answer.\n“Do not you want to know who has taken it?” cried his wife, impatiently.\n“You want to tell me, and I have no objection to hearing it.”',
     ],
     tags: ["slow-burn", "enemies-to-lovers"],
@@ -28,7 +29,8 @@ export const BookInfo = [
     title: "A Tale of Two Cities",
     subtitle: "Chapter 1",
     content: [
-      "It was the best of times, it was the worst of times, it was the age of wisdom, it was the age of foolishness, it was the epoch of belief, it was the epoch of incredulity, it was the season of Light, it was the season of Darkness, it was the spring of hope, it was the winter of despair,\nwe had everything before us, we had nothing before us, we were all going direct to Heaven,",
+      "It was the best of times, it was the worst of times, it was the age of wisdom, it was the age of foolishness, it was the epoch of belief, it was the epoch of incredulity, it was the season of Light, it was the season of Darkness, it was the spring of hope, it was the winter of despair.",
+      "We had everything before us, we had nothing before us, we were all going direct to Heaven,",
       "we were all going direct the other way—in short, the period was so far like the present period, that some of its noisiest authorities insisted on its being received, for good or for evil, in the superlative degree of comparison only.",
     ],
     tags: ["anti-hero", "french-revolution", "victorian-novel"],
